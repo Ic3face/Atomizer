ccebe587f90421f7f057c90b8c63e1a95a5a980b
Ic3face <scouter070@yahoo.de>
2014-09-29 19:46:18 +0200

    Revert "Revert "a bit more working""
    This reverts commit 753f503bda763ca044e58162185849590f07b6c6.

--- js/win.js
+++ js/win.js
@@ -26,34 +26,14 @@ stats = new Stats();
 projector = new THREE.Projector();
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.bottom = '10px';
-container = document.createElement( 'div' );
-windiv = document.getElementById('playground');
-windiv.appendChild( container );
+container = document.getElementById('playground');
+
 
 /**
- * PROBE
- * START
+ * Zusammensuchen der HTML-Elemente
  */
-canvas1 = document.createElement('canvas');
-context1 = canvas1.getContext('2d');
-context1.font = "Bold 20px Arial";
-context1.fillStyle = "rgba(0,0,0,0.65)";
-context1.fillText('Hello, world!', 0, 20);
-
-texture1 = new THREE.Texture(canvas1) 
-texture1.needsUpdate = true;
-
-var spriteMaterial = new THREE.SpriteMaterial( { map: texture1} );
-
-sprite1 = new THREE.Sprite( spriteMaterial );
-sprite1.scale.set(20,10,1.0);
-sprite1.position.set( 10, 5, 0 );
-scene.add( sprite1 );	
-
-/**
- * PROBE
- * ENDE
- */
+var namebox = jQuery("#atomname");
+var descbox = jQuery("#atomdesc");
 
 
 /**
@@ -73,13 +53,7 @@ var imageSuffix = ".jpg";
 var geometry = new THREE.SphereGeometry(5, 20, 20); 
 var geokugel2 = new THREE.SphereGeometry(2, 20, 20);
 var geoverbindung = new THREE.CylinderGeometry(0.5, 0.5, 8);
-/**
- * Code für eine eventuellen SkyBox Implementierung
- *  
- * var skyBoxMesh = skyboxladen();
- */
 
- 
 /**
  * Erstellen der Oberflächenbeschaffenheit und -verhalten auf Licht
  */
@@ -97,7 +71,7 @@ var verbindung1 = new THREE.Mesh(geoverbindung, mat3);
 var ambientlight = new THREE.AmbientLight(0x888888);
 var directionalLight = new THREE.DirectionalLight(0xffffff);
 cube.name = "Sauerstoff";
-kugel2.name = "Wasserstoff2";
+kugel2.name = "Wasserstoff";
 objects.push(cube);
 objects.push(kugel2);
 objects.push(verbindung1);
@@ -131,7 +105,7 @@ var angle3 = vec1.angleTo(vec2);
 rotquat.setFromAxisAngle(vec2, angle3 );*/
 
 
-verbindung1.quaternion.multiplyQuaternions( rotquat, verbindung1.quaternion );
+//verbindung1.quaternion.multiplyQuaternions( rotquat, verbindung1.quaternion );
 
 
 /**
@@ -150,11 +124,6 @@ cube.add(verbindung1);
 scene.add(ambientlight);
 scene.add(cube);
 verbindung1.add(axis1);
-/**
- * Code für eine eventuellen SkyBox Implementierung
- *  
- * scene.add(skyBoxMesh);
- */
 lichtspiel.add(directionalLight);
 scene.add(lichtspiel); 
 
@@ -164,7 +133,7 @@ scene.add(lichtspiel);
  */
 renderer.setSize(826, 700);
 renderer.setClearColor(0x3e3e3e);
-renderer.domElement.id = "view";
+renderer.domElement.id = "cview";
 container.appendChild(renderer.domElement);
 container.appendChild( stats.domElement );
 
@@ -190,32 +159,6 @@ function render (){
 	 stats.update();/*0x3e3e3e*/
 };
 
-/**
- * Code für eine eventuellen SkyBox Implementierung
- * 
- *  Funktion
- *	Laden und erstellen der Skybox
- * 	function skyboxladen() {
- *	
- *		var directions  = ["SBF", "SBB", "SBT", "SBBo", "SBL", "SBR"];
- *		var materialArray = [];
- *		THREE.ImageUtils.crossOrigin = '';
- *		for (var i = 0; i < 6; i++)
- *			materialArray.push( new THREE.MeshBasicMaterial({
- *			
- *				map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
- *				side: THREE.BackSide
- *		}));
- *	 
- *		var skyGeometry = new THREE.BoxGeometry( 500, 500, 500 );
- *		var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
- *		var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
- *	
- *		return skyBox;
- *	}
- */
-
-
 
 /**
  * Klickfunktion die vom EventListener aufgerufen wird
@@ -224,10 +167,11 @@ function klicksteuerung( event ) {
 
 	event.preventDefault();
 	
-	var poscanvasY = document.getElementById("view").offsetTop;
-	var poscanvasX = document.getElementById("view").offsetLeft;
+	var poscanvasY = document.getElementById("playground").offsetTop;
+	var poscanvasX = document.getElementById("playground").offsetLeft;
 
-	var vector = new THREE.Vector3( ( (event.clientX - poscanvasX) / 800 ) * 2 - 1, - ( (event.clientY - poscanvasY) / 600 ) * 2 + 1, 0.5 );
+
+	var vector = new THREE.Vector3( ( (event.clientX - poscanvasX) / 826 ) * 2 - 1, - ( (event.clientY - poscanvasY) / 700 ) * 2 + 1, 0.5 );
 	projector.unprojectVector( vector, camera );
 
 	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
@@ -248,11 +192,10 @@ function klicksteuerung( event ) {
 function hoversteuerung(event) {
 	
 	event.preventDefault();
-	
-	var poscanvasY = document.getElementById("view").offsetTop;
-	var poscanvasX = document.getElementById("view").offsetLeft;
-	
-	var vector = new THREE.Vector3( ( (event.clientX - poscanvasX) / 800) * 2 - 1, - ( (event.clientY - poscanvasY) / 600 ) * 2 + 1, 0.5 );
+	var poscanvasY = document.getElementById("playground").offsetTop;
+	var poscanvasX = document.getElementById("playground").offsetLeft;
+
+	var vector = new THREE.Vector3( ( (event.clientX - poscanvasX) / 826) * 2 - 1, - ( (event.clientY - poscanvasY) / 700 ) * 2 + 1, 0.5 );
 	projector.unprojectVector(vector, camera);
 	
 	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
@@ -260,31 +203,18 @@ function hoversteuerung(event) {
 	var intersects = raycaster.intersectObjects( objects );
 	
 	if ( intersects.length > 0 ) {
-		
-		/**
-		 * PROBE1
-		 *  RESTART
-		 */
+
 		if ( intersects[ 0 ].object.name )
 		{
-		    context1.clearRect(0,0,640,480);
-			var message = intersects[ 0 ].object.name;
-			var metrics = context1.measureText(message);
-			var width = metrics.width;
-			context1.fillStyle = "rgba(0,0,0,65)"; // text color
-			context1.fillText( message, 4,20 );
-			texture1.needsUpdate = true;
+			namebox.text("" + intersects[ 0 ].object.name);
+			descbox.text("" + intersects[ 0 ].object.desc);
 		}
-		else
-		{
-			context1.clearRect(0,0,300,300);
-			texture1.needsUpdate = true;
-		}
-		/**
-		 * PROBE
-		 * ENDE
-		 */
-		
+
+	}
+	else
+	{
+		namebox.text("");
+		descbox.text("");
 	}
 	
 }
