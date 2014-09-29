1c52cec6839e2f1c18738b0cff766d4661ba3bac
Ic3face <scouter070@yahoo.de>
2014-09-29 22:17:17 +0200

    Add Rotationfunction

--- js/win.js
+++ js/win.js
@@ -42,12 +42,6 @@ var descbox = jQuery("#atomdesc");
 var delta = uhr.getDelta();
 
 /**
- * Hilfsvariablen für die Bilderpfade
- */
-var imagePrefix = "bilder/";
-var imageSuffix = ".jpg";
-
-/**
  * Erstellen der Geometrie von Körpern
  */ 
 var geometry = new THREE.SphereGeometry(5, 20, 20); 
@@ -58,23 +52,27 @@ var geoverbindung = new THREE.CylinderGeometry(0.5, 0.5, 8);
  * Erstellen der Oberflächenbeschaffenheit und -verhalten auf Licht
  */
 var material = new THREE.MeshPhongMaterial({color: 0x00aa00, wireframe: false});
-var mat2 = new THREE.MeshPhongMaterial({color: 0xaa0000, wireframe: false});
-var mat3 = new THREE.MeshPhongMaterial({color: 0xcccccc, wireframe: false});
+var mat2 = new THREE.MeshPhongMaterial({color: 0xaa0000, emissive: 0x101010, shininess: 30, specular: 0x242424, wireframe: false});
+var mat3 = new THREE.MeshPhongMaterial({color: 0xcccccc, emissive: 0x101010, shininess: 40, specular: 0xaaaaaa, wireframe: false});
 
 /**
  * Erstellen der Objekte mit einer Geometrie und OFB
  */
 var cube = new THREE.Mesh(geometry, material);
 var kugel2 = new THREE.Mesh(geokugel2, mat2);
+var kugel3 = new THREE.Mesh(geokugel2, mat2);
 var lichtspiel = new THREE.Object3D();
 var verbindung1 = new THREE.Mesh(geoverbindung, mat3);
-var ambientlight = new THREE.AmbientLight(0x888888);
+var verbindung2 = new THREE.Mesh(geoverbindung, mat3);
+var ambientlight = new THREE.AmbientLight(0x555555);
 var directionalLight = new THREE.DirectionalLight(0xffffff);
 cube.name = "Sauerstoff";
 kugel2.name = "Wasserstoff";
 objects.push(cube);
 objects.push(kugel2);
+objects.push(kugel3);
 objects.push(verbindung1);
+objects.push(verbindung2);
 
 /**
  * Positionieren von Objekten
@@ -82,48 +80,40 @@ objects.push(verbindung1);
 directionalLight.position.set(5, 5, 5);
 directionalLight.target.add(cube);
 camera.position.z = 25;
-kugel2.position.set( 20, -4, 0);
+kugel2.position.set( -10, -4, 0);
+kugel3.position.set( 10, -4, 0);
+verbindung1.position.set((kugel2.position.x - cube.position.x)/2, (kugel2.position.y - cube.position.y)/2, (kugel2.position.z - cube.position.z )/2);
+verbindung2.position.set((kugel3.position.x - cube.position.x)/2, (kugel3.position.y - cube.position.y)/2, (kugel3.position.z - cube.position.z )/2);
+cube.position.set(0, 0, 0);
+
+rotbindung (cube, verbindung1);
+rotbindung (cube, verbindung2);
+
 /**
  * TEST
  * START
  */
-var axis1 = new THREE.AxisHelper(5);
+geoverbindung.parameters.height = ( new THREE.Vector3((kugel2.position.x - cube.position.x), (kugel2.position.y - cube.position.y), (kugel2.position.z - cube.position.z)).length());
+console.log(geoverbindung.parameters.height);
 
 
-var vec1 = new THREE.Vector3((kugel2.position.x - cube.position.x) , (kugel2.position.y - cube.position.y) , (kugel2.position.z - cube.position.z) );
-var rotquat = new THREE.Quaternion((kugel2.position.x - cube.position.x), (kugel2.position.y - cube.position.y), (kugel2.position.z - cube.position.z));
-/*var angle = vec1.angleTo(vec2);
-rotquat.setFromAxisAngle(vec2, angle );
-
-vec2.set( 0, 1, 0).normalize();
-var angle2  = vec1.angleTo(vec2);
-rotquat.setFromAxisAngle(vec2, angle2 );
-
-
-vec2.set(0, 0, 1).normalize();
-var angle3 = vec1.angleTo(vec2);
-rotquat.setFromAxisAngle(vec2, angle3 );*/
-
-
-//verbindung1.quaternion.multiplyQuaternions( rotquat, verbindung1.quaternion );
 
 
 /**
  * Test
  * ENDE
  */
-	
 
-verbindung1.position.set((kugel2.position.x - cube.position.x)/2, (kugel2.position.y - cube.position.y)/2, (kugel2.position.z - cube.position.z )/2);
 
 /**
  * Hinzufügen von Objekten zu der Szene
  */
 cube.add(kugel2);
+cube.add(kugel3);
 cube.add(verbindung1);
+cube.add(verbindung2);
 scene.add(ambientlight);
 scene.add(cube);
-verbindung1.add(axis1);
 lichtspiel.add(directionalLight);
 scene.add(lichtspiel); 
 
@@ -151,14 +141,34 @@ function render (){
 	requestAnimationFrame(render);
 	delta = uhr.getDelta();
 	
-	//cube.rotation.y += (delta * 5 * Math.PI / 180 );
+	cube.rotation.y += (delta * 5 * Math.PI / 180 );
 	//lichtspiel.rotation.y -= (delta * 20  * Math.PI / 180 );
 	
 	
 	 renderer.render(scene, camera);
-	 stats.update();/*0x3e3e3e*/
+	 stats.update();
 };
 
+/**
+ * Funktion für die Ausrichtung einer Verbindung zwischen 2 Atomen
+ * @param origin 
+ * @param target
+ */
+function rotbindung ( origin , target) {
+	
+	var quat = new THREE.Quaternion();
+	var vec1 = new THREE.Vector3((target.position.x - origin.position.x), (target.position.y - origin.position.y), (target.position.z - origin.position.z) );
+	var vec2 = new THREE.Vector3(0,1,0);
+	var vec3 = new THREE.Vector3(vec1.x, vec1.y, vec1.z);
+	
+	var angle = -(vec1.angleTo(vec2));
+	
+	vec3.cross(vec2);
+	vec3.normalize();
+	
+	quat.setFromAxisAngle(vec3, angle);
+	target.quaternion.multiplyQuaternions(quat, target.quaternion);
+}
 
 /**
  * Klickfunktion die vom EventListener aufgerufen wird
